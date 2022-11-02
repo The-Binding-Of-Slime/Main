4ee41d002959c5cb2a7c12a0ae60f78fafec1839
KGEW0511 <104777650+KGEW0511@users.noreply.github.com>
2022-11-02 15:07:09 +0900

    Item Move
    commit

--- Skull/Assets/Scripts/Parents/ItemDB.cs
+++ Skull/Assets/Scripts/Parents/ItemDB.cs
@@ -1,10 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-[CreateAssetMenu(fileName = "ItemDB", menuName = "Scriptable Object/ItemDB", order = int.MaxValue)]
-public class ItemDB : ScriptableObject
+[Serializable]
+public class ItemData
 {
-    [SerializeField]
-    public ItemData[] ItemData;
+    public string ItemID;
+    public GameObject gameObject;
+    public int hp;
+    public int damage;
+    public float moveSpeed;
+    public float attackDelay;
+    public float luck;
+}
+public class ItemDB : MonoBehaviour
+{
+    Dictionary<string, ItemData> ItemDictionary;
+
+    void Start()
+    {
+        ItemDictionary = new Dictionary<string, ItemData>();
+    }
 }
---
+++ Skull/Assets/Scripts/Parents/ItemUse.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemUse : MonoBehaviour
+{
+    void Update()
+    {
+        is
+    }
+}
